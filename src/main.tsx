a8e02de6f81c085811e88e2a4a8237b70df2a222
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2024-02-20 17:21:55 -0800

    s

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
-      <Routes>
-        <Route path="" element={<App />} />
-      </Routes>
+    <BrowserRouter basename="/tutoring-timesheet">
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
